189f783cfedcfe126fba0b6db325a38b7c09a284
Yikhan <yhxiaoch@gmail.com>
2019-01-04 00:43:51 +1100

    学习到cha7.2 开始图片库编辑之前

---
+++ scripts/example.js
@@ -0,0 +1,22 @@
+function insertTag(tag) {
+    //tag is the main content in <> which a html label
+    //for example: <p> where tag = p
+    return function(text) {
+        var tag_start = "<" + tag + ">";
+        var tag_end = "</" + tag + ">";
+        var str = tag_start + text + tag_end;
+        document.write(str);
+    }
+}
+
+var insertParagraph = insertTag("p");
+
+window.onload = function() {
+    var test_div = document.getElementById("testdiv");
+    var para = document.createElement("p");
+    // var info = "nodeName:" + para.nodeName + " nodeType:" + para.nodeType;
+    test_div.appendChild(para);
+    
+    var txt = document.createTextNode("Hello world");
+    para.appendChild(txt);
+}
