484aef93742648319e933dbbc42d6fd5e3fd29b6
Yikhan <yhxiaoch@gmail.com>
2019-01-05 01:16:52 +1100

    学习到chap 7.4之前

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,52 +1,3 @@
-const nodeTypeText = 3;
-
-function showPic(which_pic) {
-    // change image
-    // check if the element we try to manipulate exists
-    if (!document.getElementById("placeholder")) return false;
-    var placeholder = document.getElementById("placeholder");
-    // check if placeholder is an img file
-    if (placeholder.nodeName != "IMG") return false;
-    var source = which_pic.getAttribute("href");
-    placeholder.setAttribute("src", source);
-
-    // change the text under image
-    // check if the element we try to manipulate exists
-    if (document.getElementById("description")) {
-        var description = document.getElementById("description");
-        var text = which_pic.getAttribute("title") ? which_pic.getAttribute("title") : "";
-        if (description.firstChild.nodeType == nodeTypeText) {
-            description.firstChild.nodeValue = text;
-            return true;
-        }
-    } else {
-        return false;
-    }
-}
-
-function countBodyChildren() {
-    var body_element = document.getElementsByTagName("body")[0];
-    console.log("There are " + body_element.childNodes.length + " elements in the page");
-    //alert(body_element.childNodes);
-}
-
-function prepareGallery() {
-    // check function support of browser
-    if (!document.getElementsByTagName) return false;
-    if (!document.getElementById) return false;
-    if (!document.getElementById("image-gallery")) return false;
-    
-    var gallery = document.getElementById("image-gallery");
-    var links = gallery.getElementsByTagName("a");
-    for (var i=0; i<links.length; i++) {
-        links[i].onclick = function() {
-            // return false if showPic runs successfully
-            // 这里的false可以告诉浏览器不要执行点击后链接本来的动作，页面就不会跳转了
-            return !showPic(this);
-        }
-    }
-}
-
 // an effective way to load multiple functions when page is loaded
 function addLoadEvent(func) {
     var old_onload = window.onload;
@@ -60,4 +11,77 @@ function addLoadEvent(func) {
     }
 }
 
+function showPic(whichPic) {
+    // change image
+    // 如果没有预览区域，则返回true让浏览器自动跳转到新页面显示图片
+    if (!document.getElementById("placeholder")) return true;
+    var placeholder = document.getElementById("placeholder");
+    var source = whichPic.getAttribute("href");
+    placeholder.setAttribute("src", source);
+
+    // change the text under image
+    // check if the element we try to manipulate exists
+    if (document.getElementById("description")) {
+        var description = document.getElementById("description");
+        var text = whichPic.getAttribute("title") ? whichPic.getAttribute("title") : "";
+        description.innerHTML = text;
+    }
+    // 最后返回false防止浏览器跳转到新页面打开图片
+    return false;
+}
+
+function countBodyChildren() {
+    var bodyElement = document.getElementsByTagName("body")[0];
+    console.log("There are " + bodyElement.childNodes.length + " elements in the page");
+    //alert(bodyElement.childNodes);
+}
+
+function prepareGallery() {
+    // check function support of browser
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("image-gallery")) return false;
+    
+    var gallery = document.getElementById("image-gallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i=0; i<links.length; i++) {
+        links[i].onclick = function() {
+            // return false if showPic runs successfully
+            return showPic(this);
+        }
+    }
+}
+
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
+function preparePlaceholder() {
+    // check function support of browser
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("image-gallery")) return false;
+
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "images/placeholder.gif");
+    placeholder.setAttribute("alt", "My Image Gallery");
+
+    var description = document.createElement("p");
+    description.setAttribute("id", "description");
+    var desText = document.createTextNode("Choose an image");
+    description.appendChild(desText);
+    // insert new node
+    var gallery = document.getElementById("image-gallery");
+    insertAfter(placeholder, gallery);
+    insertAfter(description, placeholder);
+}
+
+addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
